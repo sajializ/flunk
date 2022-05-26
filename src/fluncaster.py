24a2f8079d55a153245c47e0a5e23052d363e031
SarinaHamedani <sarinahamedani3@gmail.com>
2022-05-26 13:12:11 +0430

    Add method for listening

--- src/fluncaster.py
+++ src/fluncaster.py
@@ -27,3 +27,21 @@ class Fluncaster:
         s.sendto(message.encode('utf-8'), (Fluncaster.BROADCAST_ADDRESS, Fluncaster.PORT))
 
         s.close()
+
+    def listen(self):
+        ip = ""
+        port = 12345
+
+
+        # Create a UDP socket
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        # Bind the socket to the port
+        server_address = (ip, port)
+        s.bind(server_address)
+        print("Do Ctrl+c to exit the program !!")
+
+        while True:
+            print("####### Server is listening #######")
+            data, address = s.recvfrom(4096)
+            print("\n\n 2. Server received: ", data.decode('utf-8'), "\n\n")
+            print("Address is ", address)   
