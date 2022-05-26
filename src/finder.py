7356485d42a42d48f7bf196734dd2d0b3254463c
sina-negarandeh <negarandeh.sina@gmail.com>
2022-05-26 15:31:34 +0430

    Implement get path method

--- src/finder.py
+++ src/finder.py
@@ -1,7 +1,15 @@
+import os
+
 class Finder:
+    PATH = os.path.expanduser("~/Downloads")
     
     def __init__(self) -> None:
         pass
 
-    def get_path():
-        pass
+    def get_path(self, filename):
+        result = []
+        for root, dirs, files in os.walk(self.PATH):
+            if filename in files:
+                result.append(os.path.join(root, filename))
+        return result
+
