4c1681f64be30d41205da3d8b62b229746b11672
Sajjad Alizadeh <sajjadalizadeh2000@gmail.com>
2022-05-26 05:49:45 -0400

    Add multiproccess functionality to node

--- src/node.py
+++ src/node.py
@@ -1,11 +1,20 @@
 import fluncaster
+import input_handler
+from multiprocessing import Process
 
 class Node:
 
     def __init__(self):
         self.fluncaster = fluncaster.Fluncaster()
+        self.input_handler = input_handler.InputHandler()
 
 
+if __name__ == "__main__":
+    node = Node()
+    listen_process = Process(target=node.fluncaster.listen, args=())
+    listen_process.start()
 
-n = Node()
-n.fluncaster.broadcast_request("salam.txt")
+    while True:
+        filename = node.input_handler.get_input()
+        p = Process(target=node.fluncaster.broadcast_request, args=(filename,))
+        p.start()
